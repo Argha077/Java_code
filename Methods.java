3d054b31fc57ab9e646f3b8952f6a682a77102a7
Argha Banik <arghabanik077@gmail.com>
2025-07-03 23:01:12 +0530

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,11 @@
+public class Methods {
+    public  static void main(String[] args){
+        methods();
+    }
+    static void methods(){
+        int a = 10;
+        int b = 20;
+        int sum = a+b;
+        System.out.print("Result is :" + sum);
+    }
+}
