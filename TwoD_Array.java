59133126073cd4d6be60f15314f914db9099626d
Argha Banik <arghabanik077@gmail.com>
2025-07-16 12:26:41 +0530

    Add files via upload

---
+++ TwoD_Array.java
@@ -0,0 +1,10 @@
+public class TwoD_Array {
+  public static void main (String[]args){
+      int [][] matrix = {
+              {1,2,3},
+              {4,5,6}
+      };
+      System.out.println(matrix [0][1]);
+  }
+
+}
